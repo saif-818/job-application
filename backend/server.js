61aa64926093eeb9e805a950c90b982f6b8ddf4b
PranishW <pranish2402@gmail.com>
2023-05-16 20:01:41 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ const mongoose = require("mongoose");
 const passportConfig = require("./lib/passportConfig");
 const cors = require("cors");
 const fs = require("fs");
-
+require('dotenv').config();
+const dbUrl = process.env.DB_URL
 // MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/jobPortal", {
+  .connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -41,6 +42,15 @@ app.use(passportConfig.initialize());
 // Routing
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/apiRoutes"));
+
+if(process.env.NODE_ENV =='production')
+{
+  const path = require('path')
+  app.get('/',(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,'frontend','build')))
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
